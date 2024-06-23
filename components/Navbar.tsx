ea5ad2b55c39fec2607856b6be1d58ca67bc6dd4
Sahil-Sharma-23 <8928515874.ss@gmail.com>
2024-06-23 22:45:50 +0530

    add navbar & dark mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,132 @@
-import React from "react";
+"use client";
 
-export default function Navbar() {
-  return <div>Navbar</div>;
+import { useState } from "react";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { buttonVariants } from "./ui/button";
+import { Menu } from "lucide-react";
+import { LogoIcon } from "./Icons";
+import { ModeToggle } from "./ModeToggle";
+import Link from "next/link";
+
+interface RouteProps {
+  href: string;
+  label: string;
 }
+
+const routeList: RouteProps[] = [
+  {
+    href: "#",
+    label: "Features",
+  },
+  {
+    href: "#",
+    label: "Testimonials",
+  },
+  {
+    href: "#",
+    label: "Pricing",
+  },
+  {
+    href: "#",
+    label: "FAQ",
+  },
+];
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <header className="sticky border-b-[1px] top-0 z-40 w-full overflow-hidden">
+      <NavigationMenu className="mx-auto">
+        <NavigationMenuList className="container h-14 px-4 w-screen flex justify-between ">
+          <NavigationMenuItem className="font-bold flex">
+            <Link href="/" className="ml-2 font-bold text-xl flex">
+              <LogoIcon />
+              Supa Auth
+            </Link>
+          </NavigationMenuItem>
+
+          {/* mobile */}
+          <span className="flex md:hidden">
+            <ModeToggle />
+
+            <Sheet open={isOpen} onOpenChange={setIsOpen}>
+              <SheetTrigger className="px-2">
+                <Menu
+                  className="flex md:hidden h-5 w-5"
+                  onClick={() => setIsOpen(true)}
+                >
+                  <span className="sr-only">Menu Icon</span>
+                </Menu>
+              </SheetTrigger>
+
+              <SheetContent side={"right"}>
+                <SheetHeader>
+                  <SheetTitle className="font-bold text-xl flex">
+                    <LogoIcon />
+                    Supa Auth
+                  </SheetTitle>
+                </SheetHeader>
+                <nav className="flex flex-col justify-center items-center gap-2 mt-4">
+                  {routeList.map(({ href, label }: RouteProps) => (
+                    <Link
+                      rel="noreferrer noopener"
+                      key={label}
+                      href={href}
+                      onClick={() => setIsOpen(false)}
+                      className={`w-full ${buttonVariants({
+                        variant: "ghost",
+                      })}`}
+                    >
+                      {label}
+                    </Link>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
+          </span>
+
+          {/* desktop */}
+          <nav className="hidden md:flex gap-2">
+            {routeList.map((route: RouteProps, i) => (
+              <Link
+                rel="noreferrer noopener"
+                href={route.href}
+                key={i}
+                className={`text-[17px] ${buttonVariants({
+                  variant: "ghost",
+                })}`}
+              >
+                {route.label}
+              </Link>
+            ))}
+          </nav>
+
+          <div className="hidden md:flex gap-2">
+            <Link
+              href="/auth/login"
+              className={`${buttonVariants({ variant: "secondary" })}`}
+            >
+              Login
+            </Link>
+
+            <ModeToggle />
+          </div>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </header>
+  );
+};
+
+export default Navbar;
